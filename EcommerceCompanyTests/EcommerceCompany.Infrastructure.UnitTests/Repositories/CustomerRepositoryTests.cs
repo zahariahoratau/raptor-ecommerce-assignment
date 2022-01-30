15223d9c5a2996a13cc7d2e91f8d15931cafb6fe
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2022-01-30 15:54:00 +0100

    Added unit tests for repositories

---
+++ EcommerceCompanyTests/EcommerceCompany.Infrastructure.UnitTests/Repositories/CustomerRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceCompany.Application.Repositories;
+using EcommerceCompany.Infrastructure.Repositories;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace EcommerceCompanyTests.EcommerceCompany.Infrastructure.UnitTests.Repositories
+{
+    public class CustomerRepositoryTests
+    {
+        private readonly CustomerRepository _sut;
+        private readonly Microsoft.Extensions.Logging.ILogger<CustomerRepository> _logger = Substitute.For<Microsoft.Extensions.Logging.ILogger<CustomerRepository>>();
+
+        public CustomerRepositoryTests()
+        {
+            _sut = new CustomerRepository(_logger);
+        }
+
+        [Fact]
+        public void Create_ShouldCreateCustomer_WhenCustomersIsPopulated()
+        {
+            // Arrange
+            
+        }
+    }
+}
---
+++ EcommerceCompanyTests/EcommerceCompany.Infrastructure.UnitTests/Repositories/OrderRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using AutoFixture;
+using EcommerceCompany.Application.Services;
+using EcommerceCompany.Domain.Models;
+using EcommerceCompany.Infrastructure.Repositories;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+
+namespace EcommerceCompanyTests.EcommerceCompany.Infrastructure.UnitTests.Repositories
+{
+    public class OrderRepositoryTests
+    {
+        private readonly OrderRepository _sut;
+        private readonly ILogger<OrderRepository> _logger = Substitute.For<ILogger<OrderRepository>>();
+        private readonly IDateTimeService _dateTimeService = Substitute.For<IDateTimeService>();
+
+        public OrderRepositoryTests()
+        {
+            _sut = new OrderRepository(_logger, _dateTimeService);
+        }
+
+        [Fact]
+        public void Create_ShouldCreateOrder_WhenOrderObjectIsProvided()
+        {
+            // Arrange
+            var order = new Order();
+
+            // Act
+            var result = _sut.Create(order);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}

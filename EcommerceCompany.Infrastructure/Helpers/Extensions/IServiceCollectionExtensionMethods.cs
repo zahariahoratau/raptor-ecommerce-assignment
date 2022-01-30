04429950521c82fa78a8d7aeb549000cb8627472
Zaharia Horatau-Hostiuc <1081482@ucn.dk>
2022-01-30 15:58:12 +0100

    Added useful extension methods

---
+++ EcommerceCompany.Infrastructure/Helpers/Extensions/IServiceCollectionExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using EcommerceCompany.Application.Repositories;
+using EcommerceCompany.Application.Services;
+using EcommerceCompany.Infrastructure.Repositories;
+using EcommerceCompany.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EcommerceCompany.Infrastructure.Helpers.Extensions
+{
+    public static class IServiceCollectionExtensionMethods
+    {
+        public static void AddInjectedDependencies(this IServiceCollection @this)
+        {
+            AddInjectedServices(@this);
+            AddInjectedRepositories(@this);
+        }
+
+        private static void AddInjectedServices(IServiceCollection @this)
+        {
+            @this.AddScoped<IOrderService, OrderService>();
+            @this.AddScoped<IDateTimeService, DateTimeService>();
+            @this.AddScoped<ICustomerStatusServiceFactory, CustomerStatusServiceFactory>();
+            @this.AddScoped<IDiscountService, DiscountService>();
+        }
+
+        private static void AddInjectedRepositories(IServiceCollection @this)
+        {
+            @this.AddScoped<IOrderRepository, OrderRepository>();
+            @this.AddScoped<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
---
+++ EcommerceCompany.Infrastructure/Helpers/Extensions/ObjectExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace EcommerceCompany.Infrastructure.Helpers.Extensions
+{
+    public static class ObjectExtensionMethods
+    {
+        public static void ThrowIfNull<T>(this T? @this) where T : class?
+        {
+            if (@this is null)
+                throw new ArgumentNullException($"{nameof(@this)} was null");
+        }
+    }
+}
